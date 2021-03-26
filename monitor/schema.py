f0dc32af96bcb2fad0f350e8a158681a703ee886
ZadneprovskiyNikolay <zadneprovskiyWork@gmail.com>
2021-03-26 17:34:56 +0200

    Bug fix

--- monitor/schema.py
+++ monitor/schema.py
@@ -27,14 +27,16 @@ class Query(graphene.ObjectType):
     )
 
     def resolve_devices(root, info, archive, ids=None):
+        user_id = info.context.user.id
         if ids is not None: 
-            return Device.objects.filter(pk__in=ids, is_sold=archive)             
-        return Device.objects.filter(is_sold=archive)
+            return Device.objects.filter(pk__in=ids, user=user_id, is_sold=archive)             
+        return Device.objects.filter(user=user_id, is_sold=archive)
     
     def resolve_transactions(root, info, ids=None): 
+        user_id = info.context.user.id
         if ids is not None: 
-            return Transaction.objects.filter(pk__in=ids)
-        return Transaction.objects.all()
+            return Transaction.objects.filter(user=user_id, pk__in=ids)
+        return Transaction.objects.filter(user=user_id)
 
 schema = graphene.Schema(query=Query)   
         
